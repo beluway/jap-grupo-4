3d63642a1019e14bb91f1ce3c1368f363613ad3f
akihito <akihito2000h@gmail.com>
2025-10-16 15:03:04 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,23 @@
+import { getUsuario } from "./clases/Usuario.js";
+
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById("autos").addEventListener("click", function() {
+        localStorage.setItem("catID", 101);
+        window.location = "products.html"
+    });
+    document.getElementById("juguetes").addEventListener("click", function() {
+        localStorage.setItem("catID", 102);
+        window.location = "products.html"
+    });
+    document.getElementById("muebles").addEventListener("click", function() {
+        localStorage.setItem("catID", 103);
+        window.location = "products.html"
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () { 
+
+    const usuario = getUsuario();
+    const userNameElement = document.getElementById("userName");
+    userNameElement.textContent = usuario.email;
+    });
